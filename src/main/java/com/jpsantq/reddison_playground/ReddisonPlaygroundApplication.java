1e9f1421785a45398a49f52f3f16d5933b58a83b
Juan Piero Santisteban Quiroz <jpsantq@gmail.com>
2024-11-08 10:56:38 -0500

    initial commit.

---
+++ src/main/java/com/jpsantq/reddison_playground/ReddisonPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.jpsantq.reddison_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddisonPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReddisonPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpsantq/reddison_playground/ReddisonPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpsantq.reddison_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddisonPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
