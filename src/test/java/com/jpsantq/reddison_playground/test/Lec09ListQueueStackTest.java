520ced714f88da906e63d0b2c62deeef2d46a45a
Juan Piero Santisteban Quiroz <jpsantq@gmail.com>
2024-11-10 09:58:39 -0500

    add: using Queue.

--- src/test/java/com/jpsantq/reddison_playground/test/Lec09ListQueueStackTest.java
+++ src/test/java/com/jpsantq/reddison_playground/test/Lec09ListQueueStackTest.java
@@ -3,7 +3,9 @@ package com.jpsantq.reddison_playground.test;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RListReactive;
+import org.redisson.api.RQueueReactive;
 import org.redisson.client.codec.LongCodec;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.List;
@@ -24,6 +26,24 @@ class Lec09ListQueueStackTest extends BaseTest {
                 .verifyComplete();
         StepVerifier.create(list.size())
                 .expectNext(10)
+                .verifyComplete();
+    }
+
+
+    @Test
+    void queueTest() {
+        RQueueReactive<Long> queue = client.getQueue("number-input", LongCodec.INSTANCE);
+
+        Mono<Void> queuePool = queue.poll()
+                .repeat(3)
+                .doOnNext(log::info)
+                .then();
+
+        StepVerifier.create(queuePool)
+                .verifyComplete();
+
+        StepVerifier.create(queue.size())
+                .expectNext(6)
                 .verifyComplete();
     }
 }
