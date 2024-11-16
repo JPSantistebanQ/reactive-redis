6f299f022b84265e06deb274280630b28ca3e6f0
Juan Piero Santisteban Quiroz <jpsantq@gmail.com>
2024-11-16 13:30:00 -0500

    add: using Subscriber Pattern.

--- src/test/java/com/jpsantq/reddison_playground/test/Lec12PubSubTest.java
+++ src/test/java/com/jpsantq/reddison_playground/test/Lec12PubSubTest.java
@@ -2,6 +2,7 @@ package com.jpsantq.reddison_playground.test;
 
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RPatternTopicReactive;
 import org.redisson.api.RTopicReactive;
 import org.redisson.client.codec.StringCodec;
 
@@ -24,6 +25,14 @@ class Lec12PubSubTest extends BaseTest {
         topic.getMessages(String.class)
                 .doOnNext(log::info)
                 .doOnError(log::error)
+                .subscribe();
+        sleep(60);
+    }
+
+    @Test
+    void subscriber3() {
+        RPatternTopicReactive topic = client.getPatternTopic("slack-room*", StringCodec.INSTANCE);
+        topic.addListener(String.class, (charSequence, s, mes) -> log.info("Pattern: {} -> {} {}", charSequence, s, mes))
                 .subscribe();
         sleep(60);
     }
